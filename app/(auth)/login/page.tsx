cfa2e429c4705da94711936b8124e250fbb4040c
prasad pund <prasadpund2020@gmail.com>
2025-07-02 17:52:55 +0530

    Add invoice model with user and item interfaces; update login page imports

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,6 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
 import {signIn } from "@/lib/auth";
 import SubmitButton from "@/components/SubmitButton";
 import { auth } from "@/lib/auth";
