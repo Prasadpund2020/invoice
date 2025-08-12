639722643f2e33f9840a10cbd75957058678ad10
Prasadpund2020 <prasadpund2020@gmail.com>
2025-08-12 16:24:44 +0530

    added delete function

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,14 +1,14 @@
-// app/api/products/[id]/route.ts
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/connectDB";
 import { auth } from "@/lib/auth";
 import Product from "@/models/products";
 
-interface Params {
-  params: { id: string };
-}
+export async function DELETE(
+  req: Request,
+  context: { params: Promise<{ id: string }> }
+) {
+  const params = await context.params; // await params here
 
-export async function DELETE(req: Request, { params }: Params) {
   const session = await auth();
   if (!session?.user?.id) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
